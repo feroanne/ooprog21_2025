603dbc3967276ada16a1ae1d59db7d422a05a785
BianciFeRoanneSantoya-UCM <bianciferoannes@gmail.com>
2025-09-18 09:01:02 +0800

    Add files via upload

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class UsingJOptionPaneDialogs {
+    public static void main(String[] args) {
+        while (true) {
+            String name = JOptionPane.showInputDialog(null, "Please enter your name:", "Name Input", JOptionPane.QUESTION_MESSAGE);
+            
+            if (name == null) {
+                break;
+            }
+
+            if (name.trim().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "You haven't entered a name. Please provide your name to continue.", "Input Required", JOptionPane.WARNING_MESSAGE);
+                continue;
+            }
+
+            int confirm = JOptionPane.showConfirmDialog(null,
+                    "Would you like your name to be displayed?",
+                    "Show Name Confirmation",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+
+            if (confirm == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Hello, " + name + "! Nice to meet you.", "Name Display", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            } else if (confirm == JOptionPane.NO_OPTION) {
+                continue;
+            } else {
+                System.exit(0);
+            }
+        }
+    }
+}
