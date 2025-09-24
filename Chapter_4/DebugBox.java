df886252e091cd14638fe71c206794dc749f4651
BianciFeRoanneSantoya-UCM <bianciferoannes@gmail.com>
2025-09-25 01:05:41 +0800

    Update DebugBox.java

--- Chapter_4/DebugBox.java
+++ Chapter_4/DebugBox.java
@@ -1,49 +1,50 @@
-import java.util.Scanner;
-
 public class DebugBox {
     private int width;
     private int length;
     private int height;
 
+    // Default constructor
     public DebugBox() {
         length = 1;
         width = 1;
         height = 1;
     }
 
+    // Constructor with parameters
     public DebugBox(int width, int length, int height) {
         this.width = width;
+        // Wrong: this.length = width; this.height = width;
+        // Fixed: assign properly
         this.length = length;
         this.height = height;
     }
 
+    // Method to show box dimensions
     public void showData() {
-        System.out.println("Width: " + width + "\nLength: " + length + "\nHeight: " + height);
+        System.out.println("Width: " + width + "  Length: " + length + "  Height: " + height);
     }
 
-    public int getVolume() {
+    // Wrong: public getVolume() { ... }
+    //    - No return type declared
+    // Fixed:
+    public double getVolume() {
         double vol = length * width * height;
-        return (int) vol;
+        return vol;
     }
 
-    // Main method to test DebugBox class
+     // Main method to test
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        DebugBox box1 = new DebugBox();        // uses default constructor
+        DebugBox box2 = new DebugBox(2, 4, 6); // uses parameterized constructor
+        
+        System.out.println("Box 1:");
+        box1.showData();
+        System.out.println("Volume: " + box1.getVolume());
 
-        // Prompt user for box dimensions
-        System.out.print("Enter the width of the box: ");
-        int widthInput = scanner.nextInt();
-        System.out.print("Enter the length of the box: ");
-        int lengthInput = scanner.nextInt();
-        System.out.print("Enter the height of the box: ");
-        int heightInput = scanner.nextInt();
-
-        // Create DebugBox object
-        DebugBox box = new DebugBox(widthInput, lengthInput, heightInput);
-
-        // Display results
-        box.showData();
-        System.out.println("Volume: " + box.getVolume());
+        System.out.println("\nBox 2:");
+        box2.showData();
+        System.out.println("Volume: " + box2.getVolume());
     }
 }
+
 
