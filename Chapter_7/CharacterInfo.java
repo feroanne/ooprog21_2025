9beb71d95586c8541101f779cd8b8025a529a307
BianciFeRoanneSantoya-UCM <bianciferoannes@gmail.com>
2025-10-30 13:23:24 +0800

    Update CharacterInfo.java

--- Chapter_7/CharacterInfo.java
+++ Chapter_7/CharacterInfo.java
@@ -1,1 +1,29 @@
+public class CharacterInfo {
+    public static void main(String[] args) {
+        char aChar = 'C';
+        System.out.println("The character is " + aChar);
 
+        if (Character.isUpperCase(aChar))
+            System.out.println(aChar + " is uppercase");
+        else
+            System.out.println(aChar + " is not uppercase");
+
+        if (Character.isLowerCase(aChar))
+            System.out.println(aChar + " is lowercase");
+        else
+            System.out.println(aChar + " is not lowercase");
+
+        System.out.println("After toLowerCase(), aChar is " + Character.toLowerCase(aChar));
+        System.out.println("After toUpperCase(), aChar is " + Character.toUpperCase(aChar));
+
+        if (Character.isLetterOrDigit(aChar))
+            System.out.println(aChar + " is a letter or digit");
+        else
+            System.out.println(aChar + " is neither a letter nor digit");
+
+        if (Character.isWhitespace(aChar))
+            System.out.println(aChar + " is whitespace");
+        else
+            System.out.println(aChar + " is not whitespace");
+    }
+}
